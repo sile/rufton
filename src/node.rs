200ca60f76176e8fc066dfeec4fe5213a3d1eb5f
Takeru Ohta <phjgt308@gmail.com>
2026-01-11 11:17:53 +0900

    refactor: improve JsonLinesRpcTransport trait with generic messages

--- src/node.rs
+++ src/node.rs
@@ -1,17 +1,20 @@
 #[derive(Debug)]
-pub struct RaftNode<S, T> {
-    pub storage: S,
-    pub transport: T,
+pub struct RaftNode<Storage, Transport> {
+    pub storage: Storage,
+    pub transport: Transport,
 }
 
 pub trait JsonLinesStorage {}
 
-pub type Todo = ();
+pub trait JsonLinesRpcTransport {
+    type NodeAddr;
 
-pub trait JsonRpcTransport {
-    type NodeId;
-
-    fn broadcast(&mut self, message: Todo);
-    fn send(&mut self, dst: &Self::NodeId, message: Todo);
-    fn recv(&mut self) -> Option<(Self::NodeId, Todo)>;
+    fn broadcast<T>(&mut self, message: T)
+    where
+        T: nojson::DisplayJson;
+    fn send<T>(&mut self, dst: &Self::NodeAddr, message: T)
+    where
+        T: nojson::DisplayJson;
+    fn recv(&mut self) -> Option<(Self::NodeAddr, nojson::RawJsonOwned)>;
+    fn local_addr(&self) -> &Self::NodeAddr;
 }
