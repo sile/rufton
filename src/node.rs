933a6bb524cceb1c3dbeb6d2ca391483a25f7b0c
Takeru Ohta <phjgt308@gmail.com>
2026-01-11 10:46:06 +0900

    fix: correct typo in transport field name

--- src/node.rs
+++ src/node.rs
@@ -1,8 +1,17 @@
 #[derive(Debug)]
-pub struct RaftNode<S> {
+pub struct RaftNode<S, T> {
     pub storage: S,
+    pub transpo7rt: T,
 }
 
 pub trait JsonLinesStorage {}
 
-pub trait JsonRpcServer {}
+pub type Todo = ();
+
+pub trait JsonRpcTransport {
+    type NodeId;
+
+    fn broadcast(&mut self, message: Todo);
+    fn send(&mut self, dst: &Self::NodeId, message: Todo);
+    fn recv(&mut self) -> Option<(Self::NodeId, Todo)>;
+}
