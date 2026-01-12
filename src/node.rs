7e0cbad33d9615c16752d8eb91709aebd87e4a2e
Takeru Ohta <phjgt308@gmail.com>
2026-01-12 11:15:09 +0900

    refactor: simplify JsonLinesRpcTransport trait by using NodeId directly

--- src/node.rs
+++ src/node.rs
@@ -7,14 +7,11 @@ pub struct RaftNode<Storage, Transport> {
 pub trait JsonLinesStorage {}
 
 pub trait JsonLinesRpcTransport {
-    type NodeAddr;
-
     fn broadcast<T>(&mut self, message: T)
     where
         T: nojson::DisplayJson;
-    fn send<T>(&mut self, dst: &Self::NodeAddr, message: T)
+    fn send<T>(&mut self, dst: raftbare::NodeId, message: T)
     where
         T: nojson::DisplayJson;
-    fn recv(&mut self) -> Option<(Self::NodeAddr, nojson::RawJsonOwned)>;
-    fn local_addr(&self) -> &Self::NodeAddr;
+    fn recv(&mut self) -> Option<(raftbare::NodeId, nojson::RawJsonOwned)>;
 }
