1b7e4dabfd2d084b4bfeebc38b71ac47b17914a0
Takeru Ohta <phjgt308@gmail.com>
2026-01-12 14:14:08 +0900

    refactor: simplify poll_event signature and introduce TransportEvent enum

--- src/transport.rs
+++ src/transport.rs
@@ -2,6 +2,7 @@ pub trait JsonLinesRpcTransport {
     type NodeAddr: Transferrable;
 
     fn register_node(&mut self, id: raftbare::NodeId, addr: Self::NodeAddr);
+
     fn deregister_node(&mut self, id: raftbare::NodeId);
 
     fn broadcast<T>(&mut self, message: T)
@@ -12,10 +13,7 @@ pub trait JsonLinesRpcTransport {
     where
         T: nojson::DisplayJson;
 
-    fn poll_event(
-        &mut self,
-        timeout: Option<std::time::Duration>,
-    ) -> std::io::Result<Option<(raftbare::NodeId, nojson::RawJsonOwned)>>;
+    fn poll_event(&mut self, timeout: std::time::Duration) -> std::io::Result<TransportEvent>;
 }
 
 pub trait Transferrable:
@@ -26,4 +24,14 @@ pub trait Transferrable:
 impl<T> Transferrable for T where
     T: nojson::DisplayJson + for<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>>
 {
+}
+
+#[derive(Debug)]
+pub enum TransportEvent {
+    Recv {
+        src: raftbare::NodeId,
+        message: nojson::RawJsonOwned,
+    },
+    Timeout,
+    Interrupted,
 }
