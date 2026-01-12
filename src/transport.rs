3591cc743f5184a093d7813b7cd0c299e70aced7
Takeru Ohta <phjgt308@gmail.com>
2026-01-12 14:01:32 +0900

    refactor: simplify JsonLinesRpcTransport trait with node registration

--- src/transport.rs
+++ src/transport.rs
@@ -1,15 +1,18 @@
-pub const API_NODE_ID: raftbare::NodeId = raftbare::NodeId::new(u64::MAX);
-
 pub trait JsonLinesRpcTransport {
-    fn broadcast<T>(&mut self, message: T) -> std::io::Result<()>
+    type NodeAddr;
+
+    fn register_node(&mut self, id: raftbare::NodeId, addr: Self::NodeAddr);
+    fn deregister_node(&mut self, id: raftbare::NodeId);
+
+    fn broadcast<T>(&mut self, message: T)
     where
         T: nojson::DisplayJson;
 
-    fn send<T>(&mut self, dst: raftbare::NodeId, message: T) -> std::io::Result<()>
+    fn send<T>(&mut self, dst: raftbare::NodeId, message: T)
     where
         T: nojson::DisplayJson;
 
-    fn recv(
+    fn poll_event(
         &mut self,
         timeout: Option<std::time::Duration>,
     ) -> std::io::Result<Option<(raftbare::NodeId, nojson::RawJsonOwned)>>;
