e6b042b17301a80474ad1411cbc435f25fb3be13
Takeru Ohta <phjgt308@gmail.com>
2026-01-31 09:58:48 +0900

    refactor: simplify RaftNode struct and remove generic parameters

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,21 @@
+const UNINIT_NODE_ID: raftbare::NodeId = raftbare::NodeId::new(0);
+
 #[derive(Debug)]
-pub struct RaftNode<Storage, Transport> {
-    pub storage: Storage,
-    pub transport: Transport,
+pub struct RaftNode {
+    pub node_id: raftbare::NodeId,
 }
 
-pub trait JsonLinesStorage {}
+impl RaftNode {
+    pub fn new() -> Self {
+        Self {
+            node_id: UNINIT_NODE_ID,
+        }
+    }
+
+    pub fn initialize_cluster(&mut self) -> bool {
+        if self.node_id != UNINIT_NODE_ID {
+            return false;
+        }
+        todo!()
+    }
+}
