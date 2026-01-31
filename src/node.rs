46d31e40dde51d7db935b53b5822410b6fdeb7b4
Takeru Ohta <phjgt308@gmail.com>
2026-01-31 17:37:07 +0900

    refactor: simplify node initialization and state tracking

--- src/node.rs
+++ src/node.rs
@@ -1,44 +1,38 @@
-const UNINIT_NODE_ID: raftbare::NodeId = raftbare::NodeId::new(0);
-
 #[derive(Debug)]
 pub struct RaftNode {
     pub addr: std::net::SocketAddr,
     pub inner: raftbare::Node,
     pub machine: RaftNodeStateMachine,
     pub action_queue: std::collections::VecDeque<RaftNodeAction>,
+    pub initialized: bool,
 }
 
 impl RaftNode {
-    pub fn new(addr: std::net::SocketAddr) -> Self {
+    pub fn new(id: raftbare::NodeId, addr: std::net::SocketAddr) -> Self {
         Self {
             addr,
-            inner: raftbare::Node::start(UNINIT_NODE_ID),
+            inner: raftbare::Node::start(id),
             machine: RaftNodeStateMachine {
-                next_node_id: increment_node_id(UNINIT_NODE_ID),
+                node_addrs: [(id, addr)].into_iter().collect(),
             },
             action_queue: std::collections::VecDeque::new(),
+            initialized: false,
         }
-    }
-
-    fn is_initialized(&self) -> bool {
-        self.inner.id() != UNINIT_NODE_ID
     }
 
     pub fn init_cluster(&mut self) -> bool {
-        if self.is_initialized() {
+        if self.initialized {
             return false;
         }
 
-        let node_id = self.machine.next_node_id;
-        self.machine.next_node_id = increment_node_id(self.machine.next_node_id);
-        self.inner = raftbare::Node::start(node_id);
+        self.initialized = true;
         self.push_action(RaftNodeAction::InitMachine);
 
         true
     }
 
     pub fn add_node(&mut self) -> bool {
-        if !self.is_initialized() {
+        if !self.initialized {
             return false;
         }
 
@@ -50,7 +44,7 @@ impl RaftNode {
     }
 
     pub fn next_action(&mut self) -> Option<RaftNodeAction> {
-        if !self.is_initialized() {
+        if !self.initialized {
             return None;
         }
 
@@ -63,16 +57,12 @@ impl RaftNode {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum RaftNodeAction {
-    InitMachine,
+    InitMachine, // TODO: remove
 }
 
 #[derive(Debug)]
 pub struct RaftNodeStateMachine {
-    pub next_node_id: raftbare::NodeId,
-}
-
-fn increment_node_id(id: raftbare::NodeId) -> raftbare::NodeId {
-    raftbare::NodeId::new(id.get() + 1)
+    pub node_addrs: std::collections::BTreeMap<raftbare::NodeId, std::net::SocketAddr>,
 }
 
 #[cfg(test)]
@@ -81,11 +71,15 @@ mod tests {
 
     #[test]
     fn init_cluster() {
-        let mut node = RaftNode::new(([127, 0, 0, 1], 9000).into());
+        let mut node = RaftNode::new(id(0), ([127, 0, 0, 1], 9000).into());
         assert!(node.init_cluster());
         assert!(!node.init_cluster());
 
         assert_eq!(node.next_action(), Some(RaftNodeAction::InitMachine));
         assert_eq!(node.next_action(), None);
+    }
+
+    fn id(n: u64) -> raftbare::NodeId {
+        raftbare::NodeId::new(n)
     }
 }
