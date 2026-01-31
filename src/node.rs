ba329924781d0dcea6298ca9f12379f178d37019
Takeru Ohta <phjgt308@gmail.com>
2026-01-31 11:21:36 +0900

    refactor: restructure RaftNode with action queue and state management

--- src/node.rs
+++ src/node.rs
@@ -2,31 +2,51 @@ const UNINIT_NODE_ID: raftbare::NodeId = raftbare::NodeId::new(0);
 
 #[derive(Debug)]
 pub struct RaftNode {
-    pub node: raftbare::Node,
-    machine: RaftNodeStateMachine,
+    pub inner: raftbare::Node,
+    pub machine: RaftNodeStateMachine,
+    pub action_queue: std::collections::VecDeque<RaftNodeAction>,
 }
 
 impl RaftNode {
+
     pub fn new() -> Self {
         Self {
-            node: raftbare::Node::start(UNINIT_NODE_ID),
+            inner: raftbare::Node::start(UNINIT_NODE_ID),
             machine: RaftNodeStateMachine {
                 next_node_id: increment_node_id(UNINIT_NODE_ID),
             },
+            action_queue: std::collections::VecDeque::new(),
         }
     }
 
-    pub fn initialize_cluster(&mut self) -> bool {
-        if self.node.id() != UNINIT_NODE_ID {
+    pub fn init_cluster(&mut self) -> bool {
+        if self.inner.id() != UNINIT_NODE_ID {
             return false;
         }
 
         let node_id = self.machine.next_node_id;
         self.machine.next_node_id = increment_node_id(self.machine.next_node_id);
+        self.inner = raftbare::Node::start(node_id);
+        self.push_action(RaftNodeAction::InitMachine);
 
-        self.node = raftbare::Node::start(node_id);
         true
     }
+
+    fn push_action(&mut self, action: RaftNodeAction) {
+        for inner_action in self.inner.actions_mut() {
+            todo!("convert: {inner_action:?}");
+        }
+        self.action_queue.push_back(action);
+    }
+
+    pub fn next_action(&mut self) -> Option<RaftNodeAction> {
+        self.action_queue.pop_front()
+    }
+}
+
+#[derive(Debug)]
+pub enum RaftNodeAction {
+    InitMachine,
 }
 
 #[derive(Debug)]
