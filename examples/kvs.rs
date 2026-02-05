a4fb655b227268f575ebb8ee3911fb47669fafcf
Takeru Ohta <phjgt308@gmail.com>
2026-02-06 01:30:13 +0900

    feat: add rand dependency and kvs example

---
+++ examples/kvs.rs
@@ -0,0 +1,61 @@
+pub fn main() -> noargs::Result<()> {
+    let mut args = noargs::raw_args();
+    args.metadata_mut().app_name = "kvs";
+    args.metadata_mut().app_description = "KVS example";
+
+    noargs::HELP_FLAG.take_help(&mut args);
+
+    let port: u16 = noargs::opt("port")
+        .short('p')
+        .default("9000")
+        .take(&mut args)
+        .then(|a| a.value().parse())?;
+    let contact_node = noargs::opt("contact")
+        .short('c')
+        .take(&mut args)
+        .present_and_then(|a| a.value().parse().map(noraft::NodeId::new))?;
+
+    if let Some(help) = args.finish()? {
+        print!("{help}");
+        return Ok(());
+    }
+
+    let addr = format!("127.0.0.1:{port}");
+    run_node(&addr)?;
+    Ok(())
+}
+
+fn run_node(listen_addr: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let socket_addr: std::net::SocketAddr = listen_addr.parse()?;
+    let mut poll = mio::Poll::new()?;
+    let min_token = mio::Token(0);
+    let max_token = mio::Token(1024);
+
+    let mut server = rufton::JsonRpcServer::start(&mut poll, min_token, max_token, socket_addr)?;
+    eprintln!("Echo server listening on {}", listen_addr);
+
+    let mut events = mio::Events::with_capacity(128);
+
+    loop {
+        poll.poll(&mut events, None)?;
+
+        for event in events.iter() {
+            server.handle_mio_event(&mut poll, event)?;
+        }
+
+        while let Some((client_id, line)) = server.next_request_line() {
+            match rufton::JsonRpcRequest::parse(line) {
+                Err(e) => {
+                    server.reply_err(&mut poll, client_id, None, e.code(), e.message())?;
+                }
+                Ok(req) => {
+                    let Some(req_id) = req.id().cloned() else {
+                        continue;
+                    };
+                    let json = req.into_json().into_owned();
+                    server.reply_ok(&mut poll, client_id, &req_id, json)?;
+                }
+            }
+        }
+    }
+}
