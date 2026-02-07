7cc01bccba0257d358a0efb314613909bdec109a
Takeru Ohta <phjgt308@gmail.com>
2026-02-07 23:48:54 +0900

    refactor: extract kvs_min into helper functions

--- examples/kvs_min.rs
+++ examples/kvs_min.rs
@@ -10,109 +10,108 @@ pub fn main() -> rufton::Result<()> {
     };
     let port: u16 = arg.parse()?;
 
-    let mut kvs = Kvs::new(noraft::NodeId::new(port as u64))?;
-    kvs.run()?;
+    run(format!("127.0.0.1:{port}").parse()?)
+}
+
+fn run(addr: std::net::SocketAddr) -> rufton::Result<()> {
+    let mut socket = rufton::LineFramedTcpSocket::bind(addr)?;
+    let mut machine = KvsMachine::new();
+
+    let node_id = noraft::NodeId::new(addr.port() as u64);
+    let mut node = rufton::Node::start(node_id);
+    let members = [NodeId::new(9000), NodeId::new(9001), NodeId::new(9002)];
+    if node_id == members[0] {
+        node.init_cluster(&members);
+    }
+
+    let mut buf = [0; 65535];
+    loop {
+        while let Some(action) = node.next_action() {
+            match action {
+                rufton::Action::BroadcastMessage(msg) => {
+                    broadcast_message(&mut socket, &node, msg)?;
+                }
+                rufton::Action::SendMessage(dst, msg) => {
+                    send_message(&mut socket, dst, msg)?;
+                }
+                rufton::Action::Commit {
+                    proposal_id,
+                    command: Some(command), // TDOO: remove
+                    ..
+                } => {
+                    handle_command(&mut socket, &mut machine, proposal_id, command)?;
+                }
+                _ => todo!(),
+            }
+        }
+
+        let (len, src_addr) = socket.recv_from(&mut buf)?;
+        let text = str::from_utf8(&buf[..len])?; // TODO: note
+        let json = nojson::RawJson::parse(text)?;
+        let request = json.value();
+
+        let method: &str = request.to_member("method")?.required()?.try_into()?;
+        let params = request.to_member("params")?.required()?;
+        let id: u64 = request.to_member("id")?.required()?.try_into()?;
+
+        if method == "_message" {
+            // TODO: remove JsonLineValue
+            node.handle_message(&rufton::JsonLineValue::new(params));
+        } else {
+            let command = nojson::object(|f| {
+                f.member("method", method)?;
+                f.member("params", params)?;
+                f.member("id", id)?;
+                f.member("src", src_addr)
+            });
+            let _proposal_id = node.propose_command(rufton::JsonLineValue::new(command));
+        }
+    }
+}
+
+fn broadcast_message(
+    socket: &mut rufton::LineFramedTcpSocket,
+    node: &rufton::Node,
+    msg: rufton::JsonLineValue,
+) -> rufton::Result<()> {
+    let req = format!(r#"{{"jsonrpc":"2.0","method":"_message","params":{msg}}}"#);
+    for dst in node.peers() {
+        let addr = SocketAddr::from(([127, 0, 0, 1], dst.get() as u16));
+        socket.send_to(req.as_bytes(), addr)?;
+    }
     Ok(())
 }
 
-struct Kvs {
-    socket: rufton::LineFramedTcpSocket,
-    node: rufton::Node,
-    machine: KvsMachine,
+fn send_message(
+    socket: &mut rufton::LineFramedTcpSocket,
+    dst: noraft::NodeId,
+    msg: rufton::JsonLineValue,
+) -> rufton::Result<()> {
+    let req = format!(r#"{{"jsonrpc":"2.0","method":"_message","params":{msg}}}"#);
+    let addr = SocketAddr::from(([127, 0, 0, 1], dst.get() as u16));
+    socket.send_to(req.as_bytes(), addr)?;
+    Ok(())
 }
 
-impl Kvs {
-    fn new(node_id: noraft::NodeId) -> rufton::Result<Self> {
-        let addr = SocketAddr::from(([127, 0, 0, 1], node_id.get() as u16));
-        let socket = rufton::LineFramedTcpSocket::bind(addr)?;
-        let machine = KvsMachine::new();
+fn handle_command(
+    socket: &mut rufton::LineFramedTcpSocket,
+    machine: &mut KvsMachine,
+    proposal_id: Option<rufton::ProposalId>,
+    command: rufton::JsonLineValue,
+) -> rufton::Result<()> {
+    let v = command.get().to_member("command")?.required()?; // TODO: Remove this call
+    let method: &str = v.to_member("method")?.required()?.try_into()?;
+    let params = v.to_member("params")?.required()?;
+    let id: u64 = v.to_member("id")?.required()?.try_into()?;
+    let src: SocketAddr = v.to_member("src")?.required()?.try_into()?;
 
-        let mut node = rufton::Node::start(node_id);
-
-        let members = [NodeId::new(9000), NodeId::new(9001), NodeId::new(9002)];
-        if node_id == members[0] {
-            node.init_cluster(&members);
-        }
-
-        Ok(Self {
-            socket,
-            node,
-            machine,
-        })
+    let result = apply(machine, method, params)?;
+    if proposal_id.is_some() {
+        let res = format!(r#"{{"jsonrpc":"2.0", "id":{id}, "result":{result}}}"#);
+        socket.send_to(res.as_bytes(), src)?;
     }
 
-    fn run(&mut self) -> rufton::Result<()> {
-        let mut buf = [0; 65535];
-        loop {
-            while let Some(action) = self.node.next_action() {
-                self.handle_action(action)?;
-            }
-
-            let (len, src_addr) = self.socket.recv_from(&mut buf)?;
-            let text = str::from_utf8(&buf[..len])?; // TODO: note
-            let json = nojson::RawJson::parse(text)?;
-            let request = json.value();
-
-            let method: &str = request.to_member("method")?.required()?.try_into()?;
-            let params = request.to_member("params")?.required()?;
-            let id: u64 = request.to_member("id")?.required()?.try_into()?;
-
-            if method == "_message" {
-                // TODO: remove JsonLineValue
-                self.node
-                    .handle_message(&rufton::JsonLineValue::new(params));
-            } else {
-                let command = nojson::object(|f| {
-                    f.member("method", method)?;
-                    f.member("params", params)?;
-                    f.member("id", id)?;
-                    f.member("src", src_addr)
-                });
-                self.node
-                    .propose_command(rufton::JsonLineValue::new(command));
-            }
-        }
-    }
-
-    fn handle_action(&mut self, action: rufton::Action) -> rufton::Result<()> {
-        match action {
-            rufton::Action::BroadcastMessage(msg) => {
-                let req = format!(r#"{{"jsonrpc":"2.0","method":"_message","params":{msg}}}"#);
-                for dst in self.node.peers() {
-                    let addr = SocketAddr::from(([127, 0, 0, 1], dst.get() as u16));
-                    self.socket.send_to(req.as_bytes(), addr)?;
-                }
-            }
-            rufton::Action::SendMessage(dst, msg) => {
-                let req = format!(r#"{{"jsonrpc":"2.0","method":"_message","params":{msg}}}"#);
-                let addr = SocketAddr::from(([127, 0, 0, 1], dst.get() as u16));
-                self.socket.send_to(req.as_bytes(), addr)?;
-            }
-            rufton::Action::Commit {
-                proposal_id,
-                index,
-                command,
-            } => {
-                eprintln!("Commit: {} ({:?})", index.get(), proposal_id);
-
-                if let Some(command) = command {
-                    let v = command.get().to_member("command")?.required()?; // TODO: Remove this call
-                    let method: &str = v.to_member("method")?.required()?.try_into()?;
-                    let params = v.to_member("params")?.required()?;
-                    let id: u64 = v.to_member("id")?.required()?.try_into()?;
-                    let src: SocketAddr = v.to_member("src")?.required()?.try_into()?;
-
-                    let result = apply(&mut self.machine, method, params)?;
-                    if proposal_id.is_some() {
-                        let res = format!(r#"{{"jsonrpc":"2.0", "id":{id}, "result":{result}}}"#);
-                        self.socket.send_to(res.as_bytes(), src)?;
-                    }
-                }
-            }
-            _ => todo!(),
-        }
-        Ok(())
-    }
+    Ok(())
 }
 
 fn apply(
