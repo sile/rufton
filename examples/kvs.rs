46871beda77c274bc5e4aa3af839c632fc6224c1
Takeru Ohta <phjgt308@gmail.com>
2026-02-07 16:29:27 +0900

    refactor: move node logic into Kvs struct

--- examples/kvs.rs
+++ examples/kvs.rs
@@ -25,7 +25,7 @@ pub fn main() -> noargs::Result<()> {
         return Ok(());
     }
 
-    run_node(noraft::NodeId::new(port as u64), init_node_ids)?;
+    run(noraft::NodeId::new(port as u64), init_node_ids)?;
     Ok(())
 }
 
@@ -33,132 +33,110 @@ fn addr(id: noraft::NodeId) -> SocketAddr {
     ([127, 0, 0, 1], id.get() as u16).into()
 }
 
-fn send_request<T: nojson::DisplayJson>(
-    socket: &mut rufton::LineFramedTcpSocket,
-    dst: SocketAddr,
-    method: &str,
-    params: T,
-) -> std::io::Result<()> {
-    let method = nojson::Json(method);
-    let params = nojson::Json(params);
-    let mut buf = Vec::new();
-    write!(
-        &mut buf,
-        r#"{{"jsonrpc":"2.0","method":{method},"params":{params}}}"#,
-    )?;
-    socket.send_to(&buf, dst)?;
-    Ok(())
+struct Kvs {
+    socket: rufton::LineFramedTcpSocket,
+    node: rufton::Node,
+    machine: std::collections::HashMap<String, nojson::RawJsonOwned>,
+    storage: rufton::FileStorage,
+    requests: std::collections::HashMap<rufton::ProposalId, (SocketAddr, rufton::JsonRpcRequestId)>,
+    timeout_time: std::time::Instant,
+    buf: [u8; 65535],
 }
 
-fn send_response<T: nojson::DisplayJson>(
-    socket: &mut rufton::LineFramedTcpSocket,
-    dst: SocketAddr,
-    request_id: &rufton::JsonRpcRequestId,
-    result: T,
-) -> std::io::Result<()> {
-    let id = nojson::Json(request_id);
-    let result = nojson::Json(result);
-    let mut buf = Vec::new();
-    write!(
-        &mut buf,
-        r#"{{"jsonrpc":"2.0","id":{id},"result":{result}}}"#,
-    )?;
-    socket.send_to(&buf, dst)?;
-    Ok(())
-}
+impl Kvs {
+    fn new(
+        node_id: noraft::NodeId,
+        init_node_ids: Option<Vec<noraft::NodeId>>,
+    ) -> noargs::Result<Self> {
+        let socket = rufton::LineFramedTcpSocket::bind(addr(node_id))?;
+        eprintln!("Started node {}", node_id.get());
 
-fn run_node(
-    node_id: noraft::NodeId,
-    init_node_ids: Option<Vec<noraft::NodeId>>,
-) -> noargs::Result<()> {
-    let mut socket = rufton::LineFramedTcpSocket::bind(addr(node_id))?;
-    eprintln!("Started node {}", node_id.get());
-
-    let mut node = rufton::Node::start(node_id);
-    let mut machine = std::collections::HashMap::<String, nojson::RawJsonOwned>::new();
-    let mut storage = rufton::FileStorage::open(format!("/tmp/kvs-{}.jsonl", node_id.get()))?;
-    let entries = storage.load_entries()?;
-    if entries.is_empty() {
-        if let Some(members) = init_node_ids {
-            node.init_cluster(&members);
-        }
-    } else {
-        let (ok, snapshot) = node.load(&entries);
-        assert!(ok);
-        if let Some(snapshot) = snapshot {
-            machine = snapshot.try_into()?;
-        }
-    }
-
-    let mut timeout_time = next_timeout_time(noraft::Role::Follower);
-    let mut requests = std::collections::HashMap::new(); // TODO: use VecDeque
-    let mut buf = [0u8; 65535];
-    loop {
-        let now = std::time::Instant::now();
-        if timeout_time < now {
-            node.handle_timeout();
-        }
-
-        drain_actions(
-            &mut socket,
-            &mut storage,
-            &mut node,
-            &mut machine,
-            &mut requests,
-            &mut timeout_time,
-        )?;
-
-        let timeout = timeout_time.saturating_duration_since(now);
-        socket.set_read_timeout(Some(timeout))?;
-
-        let (len, src_addr) = match socket.recv_from(&mut buf) {
-            Ok((len, src_addr)) => (len, src_addr),
-            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => continue,
-            Err(e) => return Err(e.into()),
-        };
-
-        let req = rufton::JsonRpcRequest::parse(&buf[..len]).expect("should return err res");
-        if let Some(req_id) = req.id().cloned() {
-            assert_eq!(req.method(), "Command");
-            let params = req.params().expect("bug");
-            let proposal_id = node.propose_command(rufton::JsonLineValue::new(params));
-            requests.insert(proposal_id, (src_addr, req_id));
+        let mut node = rufton::Node::start(node_id);
+        let mut machine = std::collections::HashMap::<String, nojson::RawJsonOwned>::new();
+        let mut storage = rufton::FileStorage::open(format!("/tmp/kvs-{}.jsonl", node_id.get()))?;
+        let entries = storage.load_entries()?;
+        if entries.is_empty() {
+            if let Some(members) = init_node_ids {
+                node.init_cluster(&members);
+            }
         } else {
-            assert_eq!(req.method(), "Internal");
-            let params = req.params().expect("bug");
-            assert!(node.handle_message(&rufton::JsonLineValue::new(params)));
+            let (ok, snapshot) = node.load(&entries);
+            assert!(ok);
+            if let Some(snapshot) = snapshot {
+                machine = snapshot.try_into()?;
+            }
+        }
+
+        Ok(Self {
+            socket,
+            node,
+            machine,
+            storage,
+            requests: std::collections::HashMap::new(), // TODO: use VecDeque
+            timeout_time: next_timeout_time(noraft::Role::Follower),
+            buf: [0u8; 65535],
+        })
+    }
+
+    fn run_loop(&mut self) -> noargs::Result<()> {
+        loop {
+            let now = std::time::Instant::now();
+            if self.timeout_time < now {
+                self.node.handle_timeout();
+            }
+
+            while let Some(action) = self.node.next_action() {
+                self.handle_action(action)?;
+            }
+
+            let timeout = self.timeout_time.saturating_duration_since(now);
+            self.socket.set_read_timeout(Some(timeout))?;
+
+            let (len, src_addr) = match self.socket.recv_from(&mut self.buf) {
+                Ok((len, src_addr)) => (len, src_addr),
+                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => continue,
+                Err(e) => return Err(e.into()),
+            };
+
+            let req =
+                rufton::JsonRpcRequest::parse(&self.buf[..len]).expect("should return err res");
+            if let Some(req_id) = req.id().cloned() {
+                assert_eq!(req.method(), "Command");
+                let params = req.params().expect("bug");
+                let proposal_id = self
+                    .node
+                    .propose_command(rufton::JsonLineValue::new(params));
+                self.requests.insert(proposal_id, (src_addr, req_id));
+            } else {
+                assert_eq!(req.method(), "Internal");
+                let params = req.params().expect("bug");
+                assert!(
+                    self.node
+                        .handle_message(&rufton::JsonLineValue::new(params))
+                );
+            }
         }
     }
-}
 
-fn drain_actions(
-    socket: &mut rufton::LineFramedTcpSocket,
-    storage: &mut rufton::FileStorage,
-    node: &mut rufton::Node,
-    machine: &mut std::collections::HashMap<String, nojson::RawJsonOwned>,
-    requests: &mut std::collections::HashMap<
-        rufton::ProposalId,
-        (SocketAddr, rufton::JsonRpcRequestId),
-    >,
-    timeout_time: &mut std::time::Instant,
-) -> noargs::Result<()> {
-    while let Some(action) = node.next_action() {
+    fn handle_action(&mut self, action: rufton::Action) -> noargs::Result<()> {
         match action {
-            rufton::Action::AppendStorageEntry(x) => storage.append_entry(&x)?,
+            rufton::Action::AppendStorageEntry(x) => self.storage.append_entry(&x)?,
             rufton::Action::SendSnapshot(_dst) => {
-                // TODO: take snapshot if node.recent_commits().len() gets too long
+                // TODO: take snapshot json, split the json, send each fragment
                 unreachable!()
             }
             rufton::Action::SetTimeout(role) => {
-                *timeout_time = next_timeout_time(role);
+                self.timeout_time = next_timeout_time(role);
             }
             rufton::Action::BroadcastMessage(m) => {
-                for dst in node.peers() {
-                    send_request(socket, addr(dst), "Internal", &m)?;
+                let peers: Vec<_> = self.node.peers().collect();
+                for dst in peers {
+                    self.send_request(addr(dst), "Internal", &m)?;
                 }
             }
             rufton::Action::SendMessage(dst, m) => {
-                send_request(socket, addr(dst), "Internal", &m)?;
+                // TODO: take snapshot if node.recent_commits().len() gets too long
+                self.send_request(addr(dst), "Internal", &m)?;
             }
             rufton::Action::Commit {
                 proposal_id,
@@ -174,28 +152,67 @@ fn drain_actions(
                         "put" => {
                             let key = v.to_member("key")?.required()?.try_into()?;
                             let value = v.to_member("value")?.required()?.extract().into_owned();
-                            let old = machine.insert(key, value);
+                            let old = self.machine.insert(key, value);
                             rufton::JsonLineValue::new(nojson::object(|f| f.member("old", &old)))
                         }
                         "get" => {
                             let key: String = v.to_member("key")?.required()?.try_into()?; // TODO: dont use String
-                            let value = machine.get(&key);
+                            let value = self.machine.get(&key);
                             rufton::JsonLineValue::new(nojson::object(|f| f.member("value", value)))
                         }
                         _ => rufton::JsonLineValue::new("unknown type"),
                     };
                     if let Some((client_addr, req_id)) =
-                        proposal_id.and_then(|id| requests.remove(&id))
+                        proposal_id.and_then(|id| self.requests.remove(&id))
                     {
-                        send_response(socket, client_addr, &req_id, result)?;
+                        self.send_response(client_addr, &req_id, result)?;
                     }
                 }
             }
             rufton::Action::Query { .. } => unreachable!(), // TODO: Merge with Commit (and rename to Apply)
                                                             // TODO: Add NotifyEvent
         }
+        Ok(())
     }
-    Ok(())
+
+    fn send_request<T: nojson::DisplayJson>(
+        &mut self,
+        dst: SocketAddr,
+        method: &str,
+        params: T,
+    ) -> std::io::Result<()> {
+        let method = nojson::Json(method);
+        let params = nojson::Json(params);
+        let mut buf = Vec::new();
+        write!(
+            &mut buf,
+            r#"{{"jsonrpc":"2.0","method":{method},"params":{params}}}"#,
+        )?;
+        self.socket.send_to(&buf, dst)?;
+        Ok(())
+    }
+
+    fn send_response<T: nojson::DisplayJson>(
+        &mut self,
+        dst: SocketAddr,
+        request_id: &rufton::JsonRpcRequestId,
+        result: T,
+    ) -> std::io::Result<()> {
+        let id = nojson::Json(request_id);
+        let result = nojson::Json(result);
+        let mut buf = Vec::new();
+        write!(
+            &mut buf,
+            r#"{{"jsonrpc":"2.0","id":{id},"result":{result}}}"#,
+        )?;
+        self.socket.send_to(&buf, dst)?;
+        Ok(())
+    }
+}
+
+fn run(node_id: noraft::NodeId, init_node_ids: Option<Vec<noraft::NodeId>>) -> noargs::Result<()> {
+    let mut kvs = Kvs::new(node_id, init_node_ids)?;
+    kvs.run_loop()
 }
 
 fn next_timeout_time(role: noraft::Role) -> std::time::Instant {
